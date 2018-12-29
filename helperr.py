d6751d6a02b27502a0990a2b823267b1150d3ffd
collab82 <46164716+collab82@users.noreply.github.com>
2018-12-29 23:03:21 +0900

    Update helperr.py

--- helperr.py
+++ helperr.py
@@ -21,79 +21,71 @@ arch = {"vgg16":25088,
         "densenet121":1024,
         "alexnet":9216}
 
-def test_network(net, trainloader):
+def load_data(where  = "./flowers" ):
+        data_dir = where
+        train_dir = data_dir + '/train'
+        valid_dir = data_dir + '/valid'
+        test_dir = data_dir + '/test'
+        
+        train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                              transforms.RandomResizedCrop(224),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                   [0.229, 0.224, 0.225])])
+        
+        valid_transforms = transforms.Compose([transforms.Resize(256),
+                                               transforms.CenterCrop(224),
+                                               transforms.ToTensor(),
+                                               transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                    [0.229, 0.224, 0.225])])
+        
+        test_transforms = transforms.Compose([transforms.Resize(256), 
+                                              transforms.CenterCrop(224),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                   [0.229, 0.224, 0.225])])
+        
+        return trainloader, validloader, testloader
 
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.001)
+def nn_setup(structure='densenet121',dropout=0.5, hidden_layer1 = 512,lr = 0.001, power=gpu):
+    
+    
+    if structure == 'vgg16':
+        model = models.vgg16(pretrained=True)        
+    elif structure == 'densenet121':
+        model = models.densenet121(pretrained=True)
+    elif structure == 'alexnet':
+        model = models.alexnet(pretrained = True)
+    else:
+        print(" {} is not a valid model.Did you mean vgg16,densenet121,or alexnet?".format(structure))
+        
+    
+        
+    for param in model.parameters():
+        param.requires_grad = False
 
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
+        from collections import OrderedDict
+        classifier = nn.Sequential(OrderedDict([
+            ('dropout',nn.Dropout(dropout)),
+            ('inputs', nn.Linear(structures[structure], hidden_layer1)),
+            ('relu1', nn.ReLU()),
+            ('hidden_layer1', nn.Linear(hidden_layer1, 90)),
+            ('relu2',nn.ReLU()),
+            ('h_l2',nn.Linear(90,80)),
+            ('relu3',nn.ReLU()),
+            ('h_l3',nn.Linear(80,102)),
+            ('output', nn.LogSoftmax(dim=1))
+                          ]))
+        
+        
+        model.classifier = classifier
+        criterion = nn.NLLLoss()
+        optimizer = optim.Adam(model.classifier.parameters(), lr )
+        model.cuda()
+        
+        if torch.cuda.is_available() and power = 'gpu':
+            model.cuda()
 
-    # Create Variables for the inputs and targets
-    inputs = Variable(images)
-    targets = Variable(images)
+        return model , optimizer ,criterion
 
-    # Clear the gradients from all Variables
-    optimizer.zero_grad()
-
-    # Forward pass, then backward pass, then update weights
-    output = net.forward(inputs)
-    loss = criterion(output, targets)
-    loss.backward()
-    optimizer.step()
-
-    return True
-
-
-def imshow(image, ax=None, title=None, normalize=True):
-    """Imshow for Tensor."""
-    if ax is None:
-        fig, ax = plt.subplots()
-    image = image.numpy().transpose((1, 2, 0))
-
-    if normalize:
-        mean = np.array([0.485, 0.456, 0.406])
-        std = np.array([0.229, 0.224, 0.225])
-        image = std * image + mean
-        image = np.clip(image, 0, 1)
-
-    ax.imshow(image)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.tick_params(axis='both', length=0)
-    ax.set_xticklabels('')
-    ax.set_yticklabels('')
-
-    return ax
-
-
-def view_recon(img, recon):
-    ''' Function for displaying an image (as a PyTorch Tensor) and its
-        reconstruction also a PyTorch Tensor
-    '''
-
-    fig, axes = plt.subplots(ncols=2, sharex=True, sharey=True)
-    axes[0].imshow(img.numpy().squeeze())
-    axes[1].imshow(recon.data.numpy().squeeze())
-    for ax in axes:
-        ax.axis('off')
-        ax.set_adjustable('box-forced')
-
-def view_classify(img, ps):
-    ''' Function for viewing an image and it's predicted classes.
-    '''
-    num_classes = ps.size()[1]
-    ps = ps.data.numpy().squeeze()
-
-
-    fig, (ax1, ax2) = plt.subplots(figsize=(6,7), ncols=2)
-    ax1.imshow(img.numpy().squeeze())
-    ax1.axis('off')
-    ax2.barh(np.arange(num_classes), ps)
-    ax2.set_aspect(0.1)
-    ax2.set_yticks(np.arange(num_classes))
-    ax2.set_yticklabels(np.arange(num_classes).astype(int), size='large');
-    ax2.set_title('Class Probability')
-    ax2.set_xlim(0, 1.1)
